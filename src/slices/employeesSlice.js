2f912d8a0b596f223fb267b80f06d9f839c3e08b
Saikiran Bommagowni <saikiranbommagowni@gmail.com>
2024-04-14 18:26:19 +1000

    refactor(sampleData): change the url
    Changed the sampleData url to fetch in production.

--- src/slices/employeesSlice.js
+++ src/slices/employeesSlice.js
@@ -4,7 +4,7 @@ import axios from "axios"
 export const fetchEmployees = createAsyncThunk(
   "employees/fetchEmployees",
   async () => {
-    const response = await axios.get("sample-data.json")
+    const response = await axios.get("/sample-data.json")
     return response.data
   }
 )
